ee0c0965286aecdfd35f65aae6a7b85c246cb68d
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-01-22 23:04:46 -0800

    Add svg element

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json').then(json => {
-  
+  const svg = d3.select('body')
+                .append('svg')
+                .attr('width', 1000)
+                .attr('height', 750)
 }, error => {
 
 })
