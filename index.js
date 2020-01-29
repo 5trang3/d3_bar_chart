053c6646f3c6544cbda9736846127d0071bda566
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-01-28 16:29:25 -0800

    Add constants for svg width, svg height and bar width

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json').then(json => {
   const gdpData = json.data;
+  const svgWidth = 1000;
+  const svgHeight = 750;
+  const barWidth = 1000 / json.data.length;
   const svg = d3.select('body')
                 .append('svg')
                 .attr('width', 1000)
