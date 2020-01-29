f362998a3275327d9366ba5499c271632ef49bef
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-01-28 16:24:16 -0800

    Add constant for gdp data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json').then(json => {
+  const gdpData = json.data;
   const svg = d3.select('body')
                 .append('svg')
                 .attr('width', 1000)
