0fa657d71bfce4e67526e65d6dfac745e0688508
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-02-05 19:18:59 -0800

    Add axes to graph

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
         return Number(gdpElement[0].split('-')[0]) + 0.75
         break;
     }
-    //return Number(gdpElement[0].split('-')[0])
   })
   console.log(gdpData);
   console.log(yearRange)
   const svgWidth = 1000;
   const svgHeight = 750;
+  const paddingBottom = 50;
+  const paddingLeft = 50;
   const barWidth = 1000 / json.data.length;
   const svg = d3.select('body')
                 .append('svg')
@@ -32,11 +33,15 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
 
   const xscale = d3.scaleLinear()
                    .domain([d3.min(yearRange), d3.max(yearRange)])
-                   .range([0, svgWidth])
+                   .range([paddingLeft, svgWidth - paddingLeft])
 
   const yscale = d3.scaleLinear()
                    .domain([d3.min(gdpData, (d) => d[1]), d3.max(gdpData, (d) => d[1])])
-                   .range([0, svgHeight])
+                   .range([svgHeight - paddingBottom, paddingBottom])
+
+  const xaxis = d3.axisBottom(xscale);
+
+  const yAxis = d3.axisLeft(yscale);
 
   let dateConverter = function(date) {
     const month = date.split('')[6];
@@ -61,9 +66,9 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
      .enter()
      .append('rect')
      .attr('x', (d, i) => xscale(yearRange[i]))
-     .attr('y', (d, i) => svgHeight - yscale(d[1]))
+     .attr('y', (d, i) => yscale(d[1]))
      .attr('width', barWidth)
-     .attr('height', (d, i) => yscale(d[1]))
+     .attr('height', (d, i) => svgHeight - yscale(d[1]) - paddingBottom)
      .attr('fill', '#633A82')
      .on('mouseover', function(d, i) {
        tooltip.html('$' + d[1] + ' Billion' + '<br>' + dateConverter(d[0]));
@@ -71,7 +76,25 @@ d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
      })
      .on('mouseout', function(d, i) {
        tooltip.style('visibility', 'hidden');
-     })
+     });
+     svg.append('g')
+        .attr('transform', 'translate(0, ' + (svgHeight - paddingBottom) + ')')
+        .call(xaxis);
+
+     svg.append('g')
+        .attr('transform', 'translate(' + (paddingLeft) + ', 0)')
+        .call(yAxis);
+
+     svg.append('text')
+        .attr('transform', 'translate(' + svgWidth/2 + ', ' + (svgHeight) + ')')
+        .text('Year')
+        .style('text-anchor', 'middle')
+
+     svg.append('text')
+        .attr('transform', 'translate(75, ' + svgHeight/2 + ') rotate(-90)')
+        .text('GDP (Billions of USD)')
+        .style('text-anchor', 'middle')
+
 }, error => {
 
 })
